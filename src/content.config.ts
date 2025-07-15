8dec70837319ef9a617fbf1ab60454ba31ce10d7
Kamiko Loss <info@gloxi.net>
2025-07-16 03:36:52 +0900

    元デザインに合わせた

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,16 +2,12 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
+	schema: () => z.object({
 		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
+		//description: z.string(),
+		date: z.string(),
+		image: z.string().optional(),
 	}),
 });
 
